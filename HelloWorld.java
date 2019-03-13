277382f4dab90ea74283b52f28266cbfbc163263
swathibha <47377730+swathibha@users.noreply.github.com>
2019-03-13 15:14:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the car window.
-      System.out.println("Hell all, Our Leaders");
+      System.out.println("Hello to all, Our Leaders");
    }
 }
